b9d56bfd66b5ce82c9b4093a9f7672538b4dd524
Cloud User <centos@centosluos.novalocal>
2017-11-08 16:29:26 +0000

    add centos support

--- bin/configure_cluster.sh
+++ bin/configure_cluster.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 #echo ansible-playbook -i hosts.prod playbook.yml
 cd ${CLUOSDIR}/ansible
-ANSIBLE_HOST_KEY_CHECKING=False time  ansible-playbook  -f 10 -i hosts.prod install.yml -e 'ansible_python_interpreter=/usr/bin/python3'
-#ANSIBLE_HOST_KEY_CHECKING=False time  ansible-playbook  -f 10 -i hosts.prod install.yml # v2
+if [ -e /etc/redhat-release ] ; then
+        ANSIBLE_HOST_KEY_CHECKING=False time  ansible-playbook  -f 10 -i hosts.prod install.yml # v2
+else
+        ANSIBLE_HOST_KEY_CHECKING=False time  ansible-playbook  -f 10 -i hosts.prod install.yml -e 'ansible_python_interpreter=/usr/bin/python3'
+fi
 
